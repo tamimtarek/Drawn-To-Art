3dfe433827e45d1f802264277b4195ff287b8451
tamimtarik15 <tamimtarik15@gmail.com>
2024-04-29 12:26:29 +0600

    install React lottio

--- src/Pages/MyList/MyList.jsx
+++ src/Pages/MyList/MyList.jsx
@@ -4,17 +4,27 @@ import List from "./List";
 
 
 const MyList = () => {
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     const [items, setItems] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`http://localhost:5000/crafts/${user.email}`)
-        .then(res =>res.json())
-        .then(data => setItems(data));
-    },[user]);
+            .then(res => res.json())
+            .then(data => setItems(data));
+    }, [user]);
     return (
         <div>
             <div>
-                <h1 className="text-xl md:text-2xl lg:text-4xl mb-9 text-center font-bold mt-9"><span className="text-red-500 mb-6">My</span> Art & Craft</h1>
+                <h1 className="text-xl md:text-2xl lg:text-4xl mb-9 text-center font-bold mt-9">
+                    <span className="text-red-500 mb-6">My</span> Art & Craft</h1>
+                <form >
+                    <details  className="dropdown">
+                        <summary className="m-1 btn">Customization</summary>
+                        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                            <li><a>Yes</a></li>
+                            <li><a>No</a></li>
+                        </ul>
+                    </details>
+                </form>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mx-auto justify-center items-center">
                 {
